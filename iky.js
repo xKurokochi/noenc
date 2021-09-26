3f7e15286f2f116be26a12228c0a2c31135f13af
Takagi Chan <74761905+Niskata@users.noreply.github.com>
2021-09-26 14:01:38 +0700

    Update iky.js

--- iky.js
+++ iky.js
@@ -3739,10 +3739,7 @@ Ket : Ketik /resetgame , Untuk Mereset Permainan Yg Ada Di Grup!`, text, {contex
                if (args.length < 1) return reply(`Masukkan nama characternya contoh #genshin keqing`)
                if (genshin.includes(arg)) {
                    path = `./random/buildgi/${args[0]}.jpeg`
-                   await ikyy.sendMessage(from, path, image, {quoted: freply}).catch(err => reply(`Terjadi kesalahan!`))
-		 } catch (err) {
-       console.error(err)
-}
+                   await ikyy.sendMessage(from, path, image, {quoted: freply}).catch(err => reply(`Terjadi kesalahan!`) console.log(e))
                } else {
                    reply(`Character yang kamu cari tidak tersedia!`)
                     }
